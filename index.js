666f5a6c9020291b9d75b599a375464c0abb8412
AbdulWahidKhan315 <k.wahidkhan55@gmail.com>
2023-10-18 23:26:31 +0600

    Implemented get method for '/brands' path, and sending response to client side

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/brands',async(req,res)=>{
+            const cursor = brandCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
